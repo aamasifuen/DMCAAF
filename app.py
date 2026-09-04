377d9a92df3c47c17aba08375f442d8e08aa95c4
aamasifuen <90862228+aamasifuen@users.noreply.github.com>
2026-09-03 22:16:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,13 @@ st.sidebar.title("Parámetros")
 st.write("Elaborado por: Abraham Amasifuen")
 
 modulos=st.sidebar.selectbox("Seleccione el modulo", ["Listas", "Arreglos", "Funciones", "POO"])
+
+if modulos == "Listas":
+  st.write("Te encuentras en el modulo de Listas")
+elif modulos == "Arreglos":
+   st.write("Te encuentras en el modulo de Arreglos")
+elif modulos == "Funciones":
+  st.write("Te encuentras en el modulo de Funciones")
+else
+  st.write("Te encuentras en el modulo de POO")
+  
